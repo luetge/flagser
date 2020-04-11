luetge/flagser

commit 1a6d896328a4b3b7bc3414e45c77a728bcdcb891

Remove memory leaks (#17)
* Remove unnecessary new for output class and users of the class

Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

* Remove unnecessary new for read_graph_flagser function

Necessary to add default constructors for filtered_directed_graph_t and
directed_graph_t to make it work

Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

* Clean up directed_flag_complex and computer

* Remove unncessary news and deletes
* Remove memory leak with filtration is now correctly clean
* Moves most of arrays to vectors
* Replace passing by pointer to passing by reference where I could

Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

* Update test for python3

Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

* Fix variable length array of non-POD element

In GCC you can define the size of an array with a non-const variable

Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

Co-authored-by: julian <julian.burellaperez@heig-vd.ch>

include/output/output_classes.h
@@ -20,19 +20,19 @@ bool has_zero_filtration_and_no_explicit_output(const named_arguments_t& named_a
 	       std::string(get_argument_or_default(named_arguments, "filtration", "zero")) == "zero";
 }
 
-template <typename Complex> output_t<Complex>* get_output(const named_arguments_t& named_arguments) {
+template <typename Complex> output_t<Complex> get_output(const named_arguments_t& named_arguments) {
 	std::string output_name = "barcode";
 	auto it = named_arguments.find("out-format");
 	if (it != named_arguments.end()) { output_name = it->second; }
 
 	if (output_name == "betti" || has_zero_filtration_and_no_explicit_output(named_arguments))
-		return new betti_output_t<Complex>(named_arguments);
-	if (output_name == "barcode") return new barcode_output_t<Complex>(named_arguments);
+		return betti_output_t<Complex>(named_arguments);
+	if (output_name == "barcode") return barcode_output_t<Complex>(named_arguments);
 
-	if (output_name == "none") return new trivial_output_t<Complex>(named_arguments);
+	if (output_name == "none") return trivial_output_t<Complex>(named_arguments);
 
 #ifdef WITH_HDF5
-	if (output_name == "barcode:hdf5") return new barcode_hdf5_output_t<Complex>(named_arguments);
+	if (output_name == "barcode:hdf5") return barcode_hdf5_output_t<Complex>(named_arguments);
 #endif
 
 	std::cerr << "The output format \"" << output_name << "\" could not be found." << std::endl;

include/persistence.h
@@ -447,7 +447,7 @@ private:
 template <typename Complex> class persistence_computer_t {
 private:
 	Complex& complex;
-	output_t<Complex>* output;
+	output_t<Complex> output;
 	value_t max_filtration;
 	size_t max_entries;
 	index_t euler_characteristic = 0;
@@ -467,7 +467,7 @@ private:
 #endif
 
 public:
-	persistence_computer_t(Complex& _complex, output_t<Complex>* _output,
+	persistence_computer_t(Complex& _complex, output_t<Complex> _output,
 	                       size_t _max_entries = std::numeric_limits<size_t>::max(), int _modulus = 2,
 	                       value_t _max_filtration = std::numeric_limits<value_t>::max())
 	    : complex(_complex), output(_output), max_filtration(_max_filtration), max_entries(_max_entries),
@@ -484,7 +484,7 @@ public:
 		compute_zeroth_persistence(min_dimension, max_dimension);
 		compute_higher_persistence(min_dimension, max_dimension);
 		complex.finished();
-		output->finished(check_euler_characteristic);
+		output.finished(check_euler_characteristic);
 
 		// Sanity check whether there were any problems computing the homology
 		bool computed_full_homology = min_dimension == 0 && max_dimension == std::numeric_limits<unsigned short>::max();
@@ -553,7 +553,7 @@ protected:
 		std_algorithms::sort(edges.rbegin(), edges.rend(), greater_filtration_or_smaller_index<filtration_index_t>());
 
 		// Let the output class know that we are now computing zeroth degree barcodes
-		output->computing_barcodes_in_dimension(0);
+		output.computing_barcodes_in_dimension(0);
 
 		for (auto e : edges) {
 			const auto vertices = complex.vertices_of_edge(get_index(e));
@@ -567,7 +567,7 @@ protected:
 				if (min_dimension == 0 && get_filtration(e) > std::max(filtration_u, filtration_v)) {
 					// Check which vertex is merged into which other vertex.
 					const auto f = dset.find(u) == u ? filtration_v : filtration_u;
-					output->new_barcode(f, get_filtration(e));
+					output.new_barcode(f, get_filtration(e));
 #ifdef RETRIEVE_PERSISTENCE
 					birth_death.push_back(std::make_pair(f, get_filtration(e)));
 #endif
@@ -591,7 +591,7 @@ protected:
 
 		for (index_t index = 0; index < n; ++index) {
 			if (dset.find(index) == index) {
-				output->new_infinite_barcode(complex.filtration(0, index));
+				output.new_infinite_barcode(complex.filtration(0, index));
 				betti_number++;
 #ifdef RETRIEVE_PERSISTENCE
 				birth_death.push_back(
@@ -606,7 +606,7 @@ protected:
 #endif
 		// Report the betti number back to the complex and the output
 		complex.computation_result(0, betti_number, 0);
-		output->betti_number(betti_number, 0);
+		output.betti_number(betti_number, 0);
 
 		if (print_betti_numbers_to_console) {
 			std::cout << "\033[K"
@@ -631,7 +631,7 @@ protected:
 
 			if (dimension + 1 < min_dimension) continue;
 
-			output->computing_barcodes_in_dimension(dimension);
+			output.computing_barcodes_in_dimension(dimension);
 
 			sort_columns();
 
@@ -652,7 +652,7 @@ protected:
 #ifdef RETRIEVE_PERSISTENCE
 				betti_numbers.push_back(betti.first);
 #endif
-				output->betti_number(betti.first, betti.second);
+				output.betti_number(betti.first, betti.second);
 				euler_characteristic += (dimension & 1 ? -1 : 1) * betti.first;
 
 				if (print_betti_numbers_to_console) {
@@ -670,7 +670,7 @@ protected:
 
 			// Stop early
 			if (complex.is_top_dimension()) {
-				output->remaining_homology_is_trivial();
+				output.remaining_homology_is_trivial();
 				break;
 			}
 		}
@@ -849,7 +849,7 @@ protected:
 
 				if (iterations > max_entries) {
 					// Abort, this is too expensive
-					if (generate_output) output->skipped_column(filtration);
+					if (generate_output) output.skipped_column(filtration);
 #ifdef RETRIEVE_PERSISTENCE
 					birth_death.push_back(
 					    std::make_pair(filtration, std::numeric_limits<value_t>::signaling_NaN()));
@@ -878,7 +878,7 @@ protected:
 #endif
 				} else {
 					if (generate_output) {
-						output->new_infinite_barcode(filtration);
+						output.new_infinite_barcode(filtration);
 						betti++;
 #ifdef RETRIEVE_PERSISTENCE
 						birth_death.push_back(std::make_pair(filtration, std::numeric_limits<value_t>::infinity()));
@@ -890,7 +890,7 @@ protected:
 			found_persistence_pair:
 				value_t death = get_filtration(pivot);
 				if (generate_output && filtration != death) {
-					output->new_barcode(filtration, death);
+					output.new_barcode(filtration, death);
 #ifdef RETRIEVE_PERSISTENCE
 					birth_death.push_back(std::make_pair(filtration, death));
 #endif

src/flagser.cpp
@@ -63,12 +63,12 @@ compute_homology(filtered_directed_graph_t& graph, const named_arguments_t& name
 	for (auto subgraph : subgraphs) {
 		directed_flag_complex_compute_t complex(subgraph, named_arguments);
 
-		output->set_complex(&complex);
+		output.set_complex(&complex);
 		if (split_into_connected_components) {
-			if (component_number > 1) output->print("\n");
-			output->print("## Path component number ");
-			output->print(std::to_string(component_number));
-			output->print("\n");
+			if (component_number > 1) output.print("\n");
+			output.print("## Path component number ");
+			output.print(std::to_string(component_number));
+			output.print("\n");
 
 #ifdef INDICATE_PROGRESS
 			std::cout << "\033[K";
@@ -88,12 +88,12 @@ compute_homology(filtered_directed_graph_t& graph, const named_arguments_t& name
 #endif
 	}
 
-	if (split_into_connected_components) { output->print("\n## Total\n"); }
+	if (split_into_connected_components) { output.print("\n## Total\n"); }
 
-	output->print_aggregated_results();
+	output.print_aggregated_results();
 
     // Closes output files, prevent memory leaks
-    delete output;
+    // delete output;
 
 #ifdef RETRIEVE_PERSISTENCE
 	return complex_subgraphs;

src/ripser.cpp
@@ -575,10 +575,10 @@ int main(int argc, char** argv) {
 
 
 	vietoris_rips_complex_t<decltype(dist)> vietoris_rips_complex(dist, dim_max, modulus);
-	auto* output = get_output<decltype(vietoris_rips_complex)>(named_arguments);
-  output->set_complex(&vietoris_rips_complex);
+	auto output = get_output<decltype(vietoris_rips_complex)>(named_arguments);
+  output.set_complex(&vietoris_rips_complex);
 
 	persistence_computer_t<decltype(vietoris_rips_complex)> persistence_computer(vietoris_rips_complex, output, max_entries, modulus, threshold);
 	persistence_computer.compute_persistence(dim_min, dim_max, false);
-	output->print_aggregated_results();
+	output.print_aggregated_results();
 }

include/complex/directed_flag_complex.h
@@ -175,19 +175,25 @@ public:
 	directed_flag_complex_t(const filtered_directed_graph_t& _graph) : graph(_graph) {}
 
 public:
-	template <typename Func> void for_each_cell(Func& f, int min_dimension, int max_dimension = -1) {
-		std::array<Func*, 1> fs{&f};
+	template <typename Func> 
+    void for_each_cell(Func& f, int min_dimension, int max_dimension = -1) {
+		std::vector<Func> fs{f};
 		for_each_cell(fs, min_dimension, max_dimension);
+        // Hack because I cannot wrap the reference in the vector, well technically I could use `std::reference_wrapper`
+        // But in this case it doesn't help
+        // What I do is storing the computed value again after computation
+        f = fs[0];
 	}
 
-	template <typename Func, size_t number_of_threads>
-	void for_each_cell(std::array<Func*, number_of_threads>& fs, int min_dimension, int max_dimension = -1) {
+	template <typename Func>
+	void for_each_cell(std::vector<Func>& fs, int min_dimension, int max_dimension = -1) {
 		if (max_dimension == -1) max_dimension = min_dimension;
-		std::thread t[number_of_threads - 1];
+        size_t number_of_threads = fs.size();
+        std::vector<std::thread> t(number_of_threads - 1);
 
 		for (size_t index = 0; index < number_of_threads - 1; ++index)
 			t[index] = std::thread(&directed_flag_complex_t::worker_thread<Func>, this, number_of_threads, index,
-			                       fs[index], min_dimension, max_dimension);
+			                       std::ref(fs[index]), min_dimension, max_dimension);
 
 		// Also do work in this thread, namely the last bit
 		worker_thread(number_of_threads, number_of_threads - 1, fs[number_of_threads - 1], min_dimension,
@@ -199,7 +205,7 @@ public:
 
 private:
 	template <typename Func>
-	void worker_thread(int number_of_threads, int thread_id, Func* f, int min_dimension, int max_dimension) {
+	void worker_thread(int number_of_threads, int thread_id, Func& f, int min_dimension, int max_dimension) {
 		const size_t number_of_vertices = graph.vertex_number();
 		const size_t vertices_per_thread = graph.vertex_number() / number_of_threads;
 
@@ -211,15 +217,15 @@ private:
 
 		do_for_each_cell(f, min_dimension, max_dimension, first_position_vertices, prefix.data(), 0);
 
-		f->done();
+		f.done();
 	}
 
 	template <typename Func>
-	void do_for_each_cell(Func* f, int min_dimension, int max_dimension,
+	void do_for_each_cell(Func& f, int min_dimension, int max_dimension,
 	                      const std::vector<vertex_index_t>& possible_next_vertices, vertex_index_t* prefix,
 	                      unsigned short prefix_size = 0) {
 		// As soon as we have the correct dimension, execute f
-		if (prefix_size >= min_dimension + 1) { (*f)(prefix, prefix_size); }
+		if (prefix_size >= min_dimension + 1) { f(prefix, prefix_size); }
 
 		// If this is the last dimension we are interested in, exit this branch
 		if (prefix_size == max_dimension + 1) return;

commit fc298ea8ef8b0aeb872e54f6f3ed4827ce97e448

Fix regression introduced in 1a6d896
Polymorphism requires pointers from base class ...

Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

include/output/output_classes.h
@@ -2,6 +2,7 @@
 
 #include <cstring>
 #include <iostream>
+#include <memory>
 
 #include "../definitions.h"
 
@@ -20,19 +21,19 @@ bool has_zero_filtration_and_no_explicit_output(const named_arguments_t& named_a
 	       std::string(get_argument_or_default(named_arguments, "filtration", "zero")) == "zero";
 }
 
-template <typename Complex> output_t<Complex> get_output(const named_arguments_t& named_arguments) {
+template <typename Complex> std::unique_ptr<output_t<Complex>> get_output(const named_arguments_t& named_arguments) {
 	std::string output_name = "barcode";
 	auto it = named_arguments.find("out-format");
 	if (it != named_arguments.end()) { output_name = it->second; }
 
 	if (output_name == "betti" || has_zero_filtration_and_no_explicit_output(named_arguments))
-		return betti_output_t<Complex>(named_arguments);
-	if (output_name == "barcode") return barcode_output_t<Complex>(named_arguments);
+		return std::make_unique<betti_output_t<Complex>>(named_arguments);
+	if (output_name == "barcode") return std::make_unique<barcode_output_t<Complex>>(named_arguments);
 
-	if (output_name == "none") return trivial_output_t<Complex>(named_arguments);
+	if (output_name == "none") return std::make_unique<trivial_output_t<Complex>>(named_arguments);
 
 #ifdef WITH_HDF5
-	if (output_name == "barcode:hdf5") return barcode_hdf5_output_t<Complex>(named_arguments);
+	if (output_name == "barcode:hdf5") return std::make_unique<barcode_hdf5_output_t<Complex>>(named_arguments);
 #endif
 
 	std::cerr << "The output format \"" << output_name << "\" could not be found." << std::endl;

include/persistence.h
@@ -461,7 +461,7 @@ private:
 template <typename Complex> class persistence_computer_t {
 private:
 	Complex& complex;
-	output_t<Complex> output;
+	output_t<Complex>* output;
 	value_t max_filtration;
 	size_t max_entries;
 	index_t euler_characteristic = 0;
@@ -481,7 +481,7 @@ private:
 #endif
 
 public:
-	persistence_computer_t(Complex& _complex, output_t<Complex> _output,
+	persistence_computer_t(Complex& _complex, output_t<Complex>* _output,
 	                       size_t _max_entries = std::numeric_limits<size_t>::max(), int _modulus = 2,
 	                       value_t _max_filtration = std::numeric_limits<value_t>::max())
 	    : complex(_complex), output(_output), max_filtration(_max_filtration), max_entries(_max_entries),
@@ -503,7 +503,7 @@ public:
 		compute_zeroth_persistence(min_dimension, max_dimension);
 		compute_higher_persistence(min_dimension, max_dimension);
 		complex.finished();
-		output.finished(check_euler_characteristic);
+		output->finished(check_euler_characteristic);
 
 		// Sanity check whether there were any problems computing the homology
 		bool computed_full_homology = min_dimension == 0 && max_dimension == std::numeric_limits<unsigned short>::max();
@@ -572,7 +572,7 @@ protected:
 		std_algorithms::sort(edges.rbegin(), edges.rend(), greater_filtration_or_smaller_index<filtration_index_t>());
 
 		// Let the output class know that we are now computing zeroth degree barcodes
-		output.computing_barcodes_in_dimension(0);
+		output->computing_barcodes_in_dimension(0);
 
 		for (auto e : edges) {
 			const auto vertices = complex.vertices_of_edge(get_index(e));
@@ -586,7 +586,7 @@ protected:
 				if (min_dimension == 0 && get_filtration(e) > std::max(filtration_u, filtration_v)) {
 					// Check which vertex is merged into which other vertex.
 					const auto f = dset.find(u) == u ? filtration_v : filtration_u;
-					output.new_barcode(f, get_filtration(e));
+					output->new_barcode(f, get_filtration(e));
 #ifdef RETRIEVE_PERSISTENCE
 					birth_death.push_back(std::make_pair(f, get_filtration(e)));
 #endif
@@ -610,7 +610,7 @@ protected:
 
 		for (index_t index = 0; index < index_t(n); ++index) {
 			if (dset.find(index) == index) {
-				output.new_infinite_barcode(complex.filtration(0, index));
+				output->new_infinite_barcode(complex.filtration(0, index));
 				betti_number++;
 #ifdef RETRIEVE_PERSISTENCE
 				birth_death.push_back(
@@ -625,7 +625,7 @@ protected:
 #endif
 		// Report the betti number back to the complex and the output
 		complex.computation_result(0, betti_number, 0);
-		output.betti_number(betti_number, 0);
+		output->betti_number(betti_number, 0);
 
 		if (print_betti_numbers_to_console) {
 			std::cout << "\033[K"
@@ -650,7 +650,7 @@ protected:
 
 			if (dimension + 1 < min_dimension) continue;
 
-			output.computing_barcodes_in_dimension(dimension);
+			output->computing_barcodes_in_dimension(dimension);
 
 			sort_columns();
 
@@ -671,7 +671,7 @@ protected:
 #ifdef RETRIEVE_PERSISTENCE
 				betti_numbers.push_back(betti.first);
 #endif
-				output.betti_number(betti.first, betti.second);
+				output->betti_number(betti.first, betti.second);
 				euler_characteristic += (dimension & 1 ? -1 : 1) * betti.first;
 
 				if (print_betti_numbers_to_console) {
@@ -689,7 +689,7 @@ protected:
 
 			// Stop early
 			if (complex.is_top_dimension()) {
-				output.remaining_homology_is_trivial();
+				output->remaining_homology_is_trivial();
 				break;
 			}
 		}
@@ -870,7 +870,7 @@ protected:
 
 				if (iterations > max_entries) {
 					// Abort, this is too expensive
-					if (generate_output) output.skipped_column(filtration);
+					if (generate_output) output->skipped_column(filtration);
 #ifdef RETRIEVE_PERSISTENCE
 					birth_death.push_back(
 					    std::make_pair(filtration, std::numeric_limits<value_t>::signaling_NaN()));
@@ -899,7 +899,7 @@ protected:
 #endif
 				} else {
 					if (generate_output) {
-						output.new_infinite_barcode(filtration);
+						output->new_infinite_barcode(filtration);
 						betti++;
 #ifdef RETRIEVE_PERSISTENCE
 						birth_death.push_back(std::make_pair(filtration, std::numeric_limits<value_t>::infinity()));
@@ -913,7 +913,7 @@ protected:
 #endif
 				value_t death = get_filtration(pivot);
 				if (generate_output && filtration != death) {
-					output.new_barcode(filtration, death);
+					output->new_barcode(filtration, death);
 #ifdef RETRIEVE_PERSISTENCE
 					birth_death.push_back(std::make_pair(filtration, death));
 #endif

src/flagser.cpp
@@ -63,12 +63,12 @@ compute_homology(filtered_directed_graph_t& graph, const named_arguments_t& name
 	for (auto subgraph : subgraphs) {
 		directed_flag_complex_compute_t complex(subgraph, named_arguments);
 
-		output.set_complex(&complex);
+		output->set_complex(&complex);
 		if (split_into_connected_components) {
-			if (component_number > 1) output.print("\n");
-			output.print("## Path component number ");
-			output.print(std::to_string(component_number));
-			output.print("\n");
+			if (component_number > 1) output->print("\n");
+			output->print("## Path component number ");
+			output->print(std::to_string(component_number));
+			output->print("\n");
 
 #ifdef INDICATE_PROGRESS
 			std::cout << "\033[K";
@@ -80,20 +80,17 @@ compute_homology(filtered_directed_graph_t& graph, const named_arguments_t& name
 		}
 
 #ifdef RETRIEVE_PERSISTENCE
-		complex_subgraphs.push_back(persistence_computer_t<decltype(complex)>(complex, output, max_entries, modulus));
+		complex_subgraphs.push_back(persistence_computer_t<decltype(complex)>(complex, output.get(), max_entries, modulus));
 		complex_subgraphs.back().compute_persistence(min_dimension, max_dimension);
 #else
-		persistence_computer_t<decltype(complex)> persistence_computer(complex, output, max_entries, modulus);
+		persistence_computer_t<decltype(complex)> persistence_computer(complex, output.get(), max_entries, modulus);
 		persistence_computer.compute_persistence(min_dimension, max_dimension);
 #endif
 	}
 
-	if (split_into_connected_components) { output.print("\n## Total\n"); }
+	if (split_into_connected_components) { output->print("\n## Total\n"); }
 
-	output.print_aggregated_results();
-
-    // Closes output files, prevent memory leaks
-    // delete output;
+	output->print_aggregated_results();
 
 #ifdef RETRIEVE_PERSISTENCE
 	return complex_subgraphs;

src/ripser.cpp
@@ -577,9 +577,9 @@ int main(int argc, char** argv) {
 
 	vietoris_rips_complex_t<decltype(dist)> vietoris_rips_complex(dist, dim_max, modulus);
 	auto output = get_output<decltype(vietoris_rips_complex)>(named_arguments);
-  output.set_complex(&vietoris_rips_complex);
+    output->set_complex(&vietoris_rips_complex);
 
-	persistence_computer_t<decltype(vietoris_rips_complex)> persistence_computer(vietoris_rips_complex, output, max_entries, modulus, threshold);
+	persistence_computer_t<decltype(vietoris_rips_complex)> persistence_computer(vietoris_rips_complex, output.get(), max_entries, modulus, threshold);
 	persistence_computer.compute_persistence(dim_min, dim_max, false);
-	output.print_aggregated_results();
+	output->print_aggregated_results();
 }