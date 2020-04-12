9a84e944572c32738146e46ac358142b3112d887
julian <julian.burellaperez@heig-vd.ch>
2020-04-12 16:15:26 +0200

    Check if we're compiling with GCC
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

--- include/output/output_classes.h
+++ include/output/output_classes.h
@@ -21,7 +21,7 @@
  */
 #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 
-#if GCC_VERSION < 40900
+#if (defined(__GNUC__) || defined(__GNUG__)) && GCC_VERSION < 40900
 template <typename T, typename... Args> std::unique_ptr<T> make_unique(Args&&... args) {
 	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
