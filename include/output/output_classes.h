36065059159a34ff8e3d3720cf50b0ad4a6cc998
julian <julian.burellaperez@heig-vd.ch>
2020-04-12 16:03:34 +0200

    Refactor code
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

--- include/output/output_classes.h
+++ include/output/output_classes.h
@@ -19,14 +19,11 @@
 /* issues with compiler version and std::make_unique were discovered
  * see: https://github.com/luetge/flagser/pull/20
  */
-#define GCC_VERSION (__GNUC__ * 10000 \
-                     + __GNUC_MINOR__ * 100 \
-                     + __GNUC_PATCHLEVEL__)
+#define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 
 #if GCC_VERSION < 40900
-template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args&&... args) {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+template <typename T, typename... Args> std::unique_ptr<T> make_unique(Args&&... args) {
+	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 #endif
 
