6da749ab81d574d7891bbc5f70695a6b912d4223
REDS institute <reds-heig@users.noreply.github.com>
2020-04-13 08:53:19 +0200

    Fix regression introduced in 1a6d896 (#20)
    * Fix regression introduced in 1a6d896
    
    Polymorphism requires pointers from base class ...
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * Fix error in test/base.h
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * Add workaround for make_unique if compiler version is lower than 4.9
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * Refactor code
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * Fix issue again with make_unique
    
    Now the namespace of std is used inside of function get_output
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * Check if we're compiling with GCC
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * Check if clang is defined, because clang defines same macros as GCC
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    Co-authored-by: julian <julian.burellaperez@heig-vd.ch>

--- include/output/output_classes.h
+++ include/output/output_classes.h
@@ -2,6 +2,7 @@
 
 #include <cstring>
 #include <iostream>
+#include <memory>
 
 #include "../definitions.h"
 
@@ -15,24 +16,40 @@
 #include "barcode_hdf5.h"
 #endif
 
+/* issues with compiler version and std::make_unique were discovered
+ * see: https://github.com/luetge/flagser/pull/20
+ */
+#define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+
+// aparently clang defines also gnuc as macros, need to check first for clang
+#if !defined(__clang__) && (defined(__GNUC__) || defined(__GNUG__)) && GCC_VERSION < 40900
+template <typename T, typename... Args> std::unique_ptr<T> make_unique(Args&&... args) {
+	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+#endif
+
 bool has_zero_filtration_and_no_explicit_output(const named_arguments_t& named_arguments) {
 	return strlen(get_argument_or_default(named_arguments, "out-format", "")) == 0 &&
 	       std::string(get_argument_or_default(named_arguments, "filtration", "zero")) == "zero";
 }
 
-template <typename Complex> output_t<Complex> get_output(const named_arguments_t& named_arguments) {
+template <typename Complex> std::unique_ptr<output_t<Complex>> get_output(const named_arguments_t& named_arguments) {
+    // using std namespace because of std::make_unique workaround
+    // This restrict the scope to inside the function
+    using namespace std;
+
 	std::string output_name = "barcode";
 	auto it = named_arguments.find("out-format");
 	if (it != named_arguments.end()) { output_name = it->second; }
 
 	if (output_name == "betti" || has_zero_filtration_and_no_explicit_output(named_arguments))
-		return betti_output_t<Complex>(named_arguments);
-	if (output_name == "barcode") return barcode_output_t<Complex>(named_arguments);
+		return make_unique<betti_output_t<Complex>>(named_arguments);
+	if (output_name == "barcode") return make_unique<barcode_output_t<Complex>>(named_arguments);
 
-	if (output_name == "none") return trivial_output_t<Complex>(named_arguments);
+	if (output_name == "none") return make_unique<trivial_output_t<Complex>>(named_arguments);
 
 #ifdef WITH_HDF5
-	if (output_name == "barcode:hdf5") return barcode_hdf5_output_t<Complex>(named_arguments);
+	if (output_name == "barcode:hdf5") return make_unique<barcode_hdf5_output_t<Complex>>(named_arguments);
 #endif
 
 	std::cerr << "The output format \"" << output_name << "\" could not be found." << std::endl;
--- include/persistence.h
+++ include/persistence.h
@@ -461,7 +461,7 @@ private:
 template <typename Complex> class persistence_computer_t {
 private:
 	Complex& complex;
-	output_t<Complex> output;
+	output_t<Complex>* output;
 	value_t max_filtration;
 	size_t max_entries;
 	index_t euler_characteristic = 0;
@@ -481,7 +481,7 @@ private:
 #endif
 
 public:
-	persistence_computer_t(Complex& _complex, output_t<Complex> _output,
+	persistence_computer_t(Complex& _complex, output_t<Complex>* _output,
 	                       size_t _max_entries = std::numeric_limits<size_t>::max(), int _modulus = 2,
 	                       value_t _max_filtration = std::numeric_limits<value_t>::max())
 	    : complex(_complex), output(_output), max_filtration(_max_filtration), max_entries(_max_entries),
@@ -503,7 +503,7 @@ public:
 		compute_zeroth_persistence(min_dimension, max_dimension);
 		compute_higher_persistence(min_dimension, max_dimension);
 		complex.finished();
-		output.finished(check_euler_characteristic);
+		output->finished(check_euler_characteristic);
 
 		// Sanity check whether there were any problems computing the homology
 		bool computed_full_homology = min_dimension == 0 && max_dimension == std::numeric_limits<unsigned short>::max();
@@ -572,7 +572,7 @@ protected:
 		std_algorithms::sort(edges.rbegin(), edges.rend(), greater_filtration_or_smaller_index<filtration_index_t>());
 
 		// Let the output class know that we are now computing zeroth degree barcodes
-		output.computing_barcodes_in_dimension(0);
+		output->computing_barcodes_in_dimension(0);
 
 		for (auto e : edges) {
 			const auto vertices = complex.vertices_of_edge(get_index(e));
@@ -586,7 +586,7 @@ protected:
 				if (min_dimension == 0 && get_filtration(e) > std::max(filtration_u, filtration_v)) {
 					// Check which vertex is merged into which other vertex.
 					const auto f = dset.find(u) == u ? filtration_v : filtration_u;
-					output.new_barcode(f, get_filtration(e));
+					output->new_barcode(f, get_filtration(e));
 #ifdef RETRIEVE_PERSISTENCE
 					birth_death.push_back(std::make_pair(f, get_filtration(e)));
 #endif
@@ -610,7 +610,7 @@ protected:
 
 		for (index_t index = 0; index < index_t(n); ++index) {
 			if (dset.find(index) == index) {
-				output.new_infinite_barcode(complex.filtration(0, index));
+				output->new_infinite_barcode(complex.filtration(0, index));
 				betti_number++;
 #ifdef RETRIEVE_PERSISTENCE
 				birth_death.push_back(
@@ -625,7 +625,7 @@ protected:
 #endif
 		// Report the betti number back to the complex and the output
 		complex.computation_result(0, betti_number, 0);
-		output.betti_number(betti_number, 0);
+		output->betti_number(betti_number, 0);
 
 		if (print_betti_numbers_to_console) {
 			std::cout << "\033[K"
@@ -650,7 +650,7 @@ protected:
 
 			if (dimension + 1 < min_dimension) continue;
 
-			output.computing_barcodes_in_dimension(dimension);
+			output->computing_barcodes_in_dimension(dimension);
 
 			sort_columns();
 
@@ -671,7 +671,7 @@ protected:
 #ifdef RETRIEVE_PERSISTENCE
 				betti_numbers.push_back(betti.first);
 #endif
-				output.betti_number(betti.first, betti.second);
+				output->betti_number(betti.first, betti.second);
 				euler_characteristic += (dimension & 1 ? -1 : 1) * betti.first;
 
 				if (print_betti_numbers_to_console) {
@@ -689,7 +689,7 @@ protected:
 
 			// Stop early
 			if (complex.is_top_dimension()) {
-				output.remaining_homology_is_trivial();
+				output->remaining_homology_is_trivial();
 				break;
 			}
 		}
@@ -870,7 +870,7 @@ protected:
 
 				if (iterations > max_entries) {
 					// Abort, this is too expensive
-					if (generate_output) output.skipped_column(filtration);
+					if (generate_output) output->skipped_column(filtration);
 #ifdef RETRIEVE_PERSISTENCE
 					birth_death.push_back(
 					    std::make_pair(filtration, std::numeric_limits<value_t>::signaling_NaN()));
@@ -899,7 +899,7 @@ protected:
 #endif
 				} else {
 					if (generate_output) {
-						output.new_infinite_barcode(filtration);
+						output->new_infinite_barcode(filtration);
 						betti++;
 #ifdef RETRIEVE_PERSISTENCE
 						birth_death.push_back(std::make_pair(filtration, std::numeric_limits<value_t>::infinity()));
@@ -913,7 +913,7 @@ protected:
 #endif
 				value_t death = get_filtration(pivot);
 				if (generate_output && filtration != death) {
-					output.new_barcode(filtration, death);
+					output->new_barcode(filtration, death);
 #ifdef RETRIEVE_PERSISTENCE
 					birth_death.push_back(std::make_pair(filtration, death));
 #endif
